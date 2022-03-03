63785b4ed4cc6d6513553a6834f55fc83ca0134e
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-03-02 17:01:35 -0800

    styled, added some data

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ function App() {
     <div className="App">
       {data.map((item) => {
         return (
-          <div className="phone">
-            <div className="phoneImg">
-              <img src={item.image} alt="" />
+          <>
+            <hr />
+            <div key={item.id} className="phone">
+              <div className="phoneImg">
+                <img src={item.image} alt="" />
+              </div>
+              <div className="phoneInfo">
+                <h1>{item.name}</h1>
+                <h3>${item.price}</h3>
+              </div>
             </div>
-            <div className="phoneInfo">
-              <h1>{item.name}</h1>
-              <h3>${item.price}</h3>
-            </div>
-          </div>
+          </>
         );
       })}
     </div>
--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 export const data = [
   {
-    name: "Nokia 222",
-    price: 299,
+    name: "Iphone 13 pro Max",
+    price: 1299,
     count: 0,
     id: 1,
     image:
-      "https://static.dezeen.com/uploads/2017/02/nokia_3310_blue_mobile-phone-technology-news_sq2-213x213.jpg",
+      "https://ss7.vzw.com/is/image/VerizonWireless/141429-iphone-13-pro-max-d?bgc=f6f6f6&scl=1",
   },
   {
     name: "Iphone X",
@@ -16,19 +16,43 @@ export const data = [
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi10pUT4NDY6Af_CHEdVUWF8jdRlD3eOmxlA&usqp=CAU",
   },
   {
-    name: "Motorola",
-    price: 359,
+    name: "Google - Pixel 6 256GB",
+    price: 599,
     count: 0,
     id: 3,
     image:
-      "https://motorolaroe.vtexassets.com/arquivos/ids/156402/Moto-G50-pdp-kv-render-2-a67qhcwy.png?v=637522731699630000",
+      "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6483/6483644_sd.jpg;maxHeight=300;maxWidth=450",
   },
   {
-    name: "Ericson Sony",
-    price: 989,
+    name: "Samsung - Galaxy S22 Ultra 128GB",
+    price: 1099,
     count: 0,
     id: 4,
     image:
-      "https://www.cnet.com/a/img/resize/0f600f70a7d08e94ad52cc7e4a75b12e6b05faa3/hub/2009/06/29/1de80723-cbf2-11e2-9a4a-0291187b029a/orig-1200x900_1.jpg?auto=webp&width=768",
+      "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6494/6494471_sd.jpg;maxHeight=240;maxWidth=350",
+  },
+  {
+    name: "Galaxy Z Flip3 5G 128GB",
+    price: 699,
+    count: 0,
+    id: 4,
+    image:
+      "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6468/6468290_sd.jpg;maxHeight=240;maxWidth=350",
+  },
+  {
+    name: "Galaxy Z Fold3 5G 256GB",
+    price: 1399,
+    count: 0,
+    id: 4,
+    image:
+      "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6494/6494471_sd.jpg;maxHeight=240;maxWidth=350",
+  },
+  {
+    name: "iPhone 11",
+    price: 499,
+    count: 0,
+    id: 4,
+    image:
+      "https://pisces.bbystatic.com/image2/BestBuy_US/Gallery/SOL-55300-iphone11-106365.jpg",
   },
 ];
