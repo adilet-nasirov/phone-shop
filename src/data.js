eeb6d29d6a0c05f1fe68caa619d049ecc47dcf7a
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-03-02 16:00:53 -0800

    phone data

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    name: "Nokia 222",
+    price: 299,
+    count: 0,
+    id: 1,
+    image:
+      "https://static.dezeen.com/uploads/2017/02/nokia_3310_blue_mobile-phone-technology-news_sq2-213x213.jpg",
+  },
+  {
+    name: "Iphone X",
+    price: 599,
+    count: 0,
+    id: 2,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi10pUT4NDY6Af_CHEdVUWF8jdRlD3eOmxlA&usqp=CAU",
+  },
+  {
+    name: "Motorola",
+    price: 359,
+    count: 0,
+    id: 3,
+    image:
+      "https://motorolaroe.vtexassets.com/arquivos/ids/156402/Moto-G50-pdp-kv-render-2-a67qhcwy.png?v=637522731699630000",
+  },
+  {
+    name: "Ericson Sony",
+    price: 989,
+    count: 0,
+    id: 4,
+    image:
+      "https://www.cnet.com/a/img/resize/0f600f70a7d08e94ad52cc7e4a75b12e6b05faa3/hub/2009/06/29/1de80723-cbf2-11e2-9a4a-0291187b029a/orig-1200x900_1.jpg?auto=webp&width=768",
+  },
+];
