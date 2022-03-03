6966574d819f8b16bd8c7c2f41e6a1f16109b80b
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-03-02 16:04:03 -0800

    mapped data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ function App() {
   return (
     <div className="App">
       {data.map((item) => {
-        return <div>{item.name}</div>;
+        return (
+          <div className="phone">
+            <div className="phoneImg">
+              <img src={item.image} alt="" />
+            </div>
+            <div className="phoneInfo">
+              <h1>{item.name}</h1>
+              <h3>${item.price}</h3>
+            </div>
+          </div>
+        );
       })}
     </div>
   );
