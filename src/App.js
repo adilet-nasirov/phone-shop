0868ccb163bd0c2c4f2d6db68c1798414a292f01
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-03-03 07:41:32 -0800

    fetched api by using axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 function App() {
   const API = "https://breakingbadapi.com/api/characters";
-  const [actors, setActor] = useState([]);
-  const fetchData = async () => {
-    const responce = await fetch(API);
-    const data = await responce.json();
-    setActor(data);
+  const [actors, setActors] = useState([]);
+  const fetchData = () => {
+    axios
+      .get(API)
+      .then((responce) => {
+        setActors(responce.data);
+      })
+      .catch(console.error());
   };
   useEffect(() => {
     fetchData();
@@ -16,7 +20,7 @@ function App() {
     <div className="App">
       {actors.map((item) => {
         return (
-          <div className="actor">
+          <div className="actor" key={item.char_id}>
             <h2>{item.category}</h2>
             <img src={item.img} alt="" width={300} height={350} />
             <h1>{item.name}</h1>
