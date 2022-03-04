958cf01bbcb9e48cbb1c4ff22f3d63b819d8dea0
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-03-04 09:04:09 -0800

    finished routing

--- src/phoneApp/Navbar.jsx
+++ src/phoneApp/Navbar.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import "./App.css";
+import { Link } from "react-router-dom";
 const Navbar = () => {
+  const navStyle = {
+    color: "white",
+    textDecoration: "none",
+  };
   return (
     <nav className="navbar">
       <h3>Logo</h3>
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Phones</li>
+        <Link style={navStyle} to="/">
+          <li>Home</li>
+        </Link>
+        <Link style={navStyle} to="/phones">
+          <li>Phones</li>
+        </Link>
+        <Link style={navStyle} to="/about">
+          <li>About</li>
+        </Link>
       </ul>
     </nav>
   );
